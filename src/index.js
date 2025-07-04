9b2f142bad2fe12208315449846a5db5d5fe506b
Daniel  Hatton <spanno11@live.com>
2025-07-05 08:20:58 +1000

    Update index.js to render MyComponent as application root
    Modified src/index.js to render the new MyComponent functional component as the entry point of the React application. This change establishes the base UI component for the TRS Alliance v2.3 platform, ensuring the app properly mounts in the DOM.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import MyComponent from "./MyComponent";
+import "./index.css";  // <-- Import Tailwind CSS here
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
